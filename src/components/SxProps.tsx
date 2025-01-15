72368bfd13cd84611adc899a3f5245e3ff8c2101
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2025-01-15 16:24:25 +0530

    learned the basics of sx props

--- src/components/SxProps.tsx
+++ src/components/SxProps.tsx
@@ -1,9 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 function SxProps() {
+  const [clicked, setClicked] = useState(false);
   return (
     <Box>
-      <Typography variant="h1" sx={{ color: "primary.contrastText" }}>
+      <Typography variant="h1" sx={{ color: "primary.contrastText", mb: 2 }}>
         Hello, I am{" "}
         <Typography
           component="span"
@@ -13,6 +15,33 @@ function SxProps() {
           Surya Kumar D
         </Typography>
       </Typography>
+      <Typography variant="h2" sx={{ color: "primary.contrastText", mb: 2 }}>
+        A{" "}
+        <Typography
+          component="span"
+          variant="h2"
+          sx={{ color: "primary.main" }}
+        >
+          Full Stack Developer
+        </Typography>
+      </Typography>
+      <Button
+        onClick={() => setClicked(!clicked)}
+        variant="contained"
+        sx={[
+          {
+            bgcolor: { md: "red", xl: "purple" },
+            "&:hover": {
+              bgcolor: "info.main",
+            },
+          },
+          clicked && {
+            bgcolor: "secondary.main",
+          },
+        ]}
+      >
+        Click Me
+      </Button>
     </Box>
   );
 }
